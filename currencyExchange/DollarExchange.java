8f1878532b63f41ef3031360e6b9d9235905d937
ar89601 <ar89601@gmail.com>
2017-06-20 12:42:45 -0400

    Update and rename templateCurrencyExchange/DollarExchange.java to currencyExchange/DollarExchange.java

--- currencyExchange/DollarExchange.java
+++ currencyExchange/DollarExchange.java
@@ -2,7 +2,7 @@
  * @author Ashley.Ridout
  * @date June 3, 2017
  */
-package templateCurrencyExchange;
+package currencyExchange;
 
 public class DollarExchange extends CurrencyExchangeAlgorithm {
 	double dollar = 1;
