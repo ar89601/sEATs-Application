83c8f604ce426e0c0f5c88494ed359dea5f17bf7
ar89601 <ar89601@gmail.com>
2017-06-20 11:34:09 -0400

    Create Order.java

---
+++ abstractFactoryOrderStyle/Order.java
@@ -0,0 +1,15 @@
+/**
+ * @author Ashley.Ridout
+ * @date May 20, 2017
+ *
+ */
+package abstractFactoryOrderStyle;
+
+//interface to hold order behavior
+public interface Order {
+
+	String getTitle();
+
+	String getName();
+
+}
