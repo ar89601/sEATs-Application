b21d7881a323ddfe331d609c7edd7964471ba5d7
ar89601 <ar89601@gmail.com>
2017-06-20 17:35:10 -0400

    Update Application.java

--- orderStyle/Application.java
+++ orderStyle/Application.java
@@ -5,38 +5,23 @@
  */
 package orderStyle;
 
-import java.util.Scanner;
-
+// class to build the order
 public class Application {
+	private Order order;
 
-	// main method to create order based on user selection
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String input;
-		Client client = new Client();
-		OrderStyle orderStyle = null;
-
-		// output to console
-		System.out.println("Please select if you would like to order by Menu by typing 'menu' or by Vendor by typing 'vendor'.");
-		input = scan.nextLine();
-
-		// if user wants to order by menu
-		if ("menu".equalsIgnoreCase(input)) {
-			orderStyle = new MenuOrderStyle();
-			client.buildOrder(orderStyle);
-			System.out.println("You have chosen to order by menu.\n" + MenuOrder.TITLE + "\n" + MenuOrder.NAME);
-		}
-		// if user wants to order by vendor
-		else if ("vendor".equalsIgnoreCase(input)) {
-			orderStyle = new VendorOrderStyle();
-			client.buildOrder(orderStyle);
-			System.out.println("You have chosen to order by vendor.\n" + VendorOrder.TITLE +"\n" + VendorOrder.NAME);
-
-		// all other input is incorrect
-		} else {
-			System.out.println("Please retry by typing 'Menu' or 'Vendor'.");
-			System.exit(0);
-		}
-		scan.close();
+	public void buildOrder(final OrderStyle style){
+		setOrder(style.createOrder());
+	}
+	
+	Order getOrder(final OrderStyle style){
+		return style.createOrder();
+	}
+	
+	public Order getOrder(){
+		return order;
+	}
+	
+	private void setOrder(final Order order){
+		this.order = order;
 	}
 }
