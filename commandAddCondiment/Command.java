0481f2dd87bdcbc9999e8072d9d2d23c02875276
ar89601 <ar89601@gmail.com>
2017-06-20 12:02:33 -0400

    Create Command.java

---
+++ commandAddCondiment/Command.java
@@ -0,0 +1,10 @@
+/**
+ * @author Ashley.Ridout
+ * @date June 5, 2017
+ */
+package commandAddCondiment;
+
+// interface to hold execute method
+public interface Command {
+	public void execute();
+}
