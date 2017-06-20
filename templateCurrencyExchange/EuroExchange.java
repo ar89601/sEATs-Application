d4eb7ead5e187474c2db19f73499e28575e66463
ar89601 <ar89601@gmail.com>
2017-06-20 11:58:25 -0400

    Create EuroExchange.java

---
+++ templateCurrencyExchange/EuroExchange.java
@@ -0,0 +1,33 @@
+/**
+ * @author Ashley.Ridout
+ * @date June 3, 2017
+ */
+package templateCurrencyExchange;
+
+public class EuroExchange extends CurrencyExchangeAlgorithm {
+	double euro = 0.89;
+	
+	public double getEuro(){
+		return euro;
+	}
+	
+	@Override
+	void displayExchangeRate(){
+		System.out.println("The exchange rate for dollar:euro is 1:0.89.");
+	}
+	
+	
+	@Override
+	void exchange(double price){
+		this.price = price;
+		double newPrice = price * euro;	
+	}	
+	
+	@Override
+	void print(double price){
+		this.price = price;
+		double newPrice = price * euro;
+		System.out.println("Your order has been changed from $" + price + " to €" + newPrice);
+	}
+	
+}
