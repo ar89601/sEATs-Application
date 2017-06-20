e7fa3df53725889d9630654863ad84420e175c30
ar89601 <ar89601@gmail.com>
2017-06-20 16:58:20 -0400

    Update Application.java

--- orderStyle/Application.java
+++ orderStyle/Application.java
@@ -17,24 +17,24 @@ public class Application {
 		OrderStyle orderStyle = null;
 
 		// output to console
-		System.out.println("Please select if you would like to order by Menu by typing 'M' or by Vendor by typing 'V'.");
+		System.out.println("Please select if you would like to order by Menu by typing 'menu' or by Vendor by typing 'vendor'.");
 		input = scan.nextLine();
 
 		// if user wants to order by menu
-		if ("m".equalsIgnoreCase(input)) {
+		if ("menu".equalsIgnoreCase(input)) {
 			orderStyle = new MenuOrderStyle();
 			client.buildOrder(orderStyle);
 			System.out.println("You have chosen to order by menu.\n" + MenuOrder.TITLE + "\n" + MenuOrder.NAME);
 		}
 		// if user wants to order by vendor
-		else if ("v".equalsIgnoreCase(input)) {
+		else if ("vendor".equalsIgnoreCase(input)) {
 			orderStyle = new VendorOrderStyle();
 			client.buildOrder(orderStyle);
 			System.out.println("You have chosen to order by vendor.\n" + VendorOrder.TITLE +"\n" + VendorOrder.NAME);
 
 		// all other input is incorrect
 		} else {
-			System.out.println("Please retry by typing 'M' or 'V'.");
+			System.out.println("Please retry by typing 'Menu' or 'Vendor'.");
 			System.exit(0);
 		}
 		scan.close();
