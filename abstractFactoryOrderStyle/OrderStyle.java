39f4eedd244340d4964e661b9c7a24215e620034
ar89601 <ar89601@gmail.com>
2017-06-20 11:34:36 -0400

    Create OrderStyle.java

---
+++ abstractFactoryOrderStyle/OrderStyle.java
@@ -0,0 +1,12 @@
+/**
+ * @author Ashley.Ridout
+ * @date May 20, 2017
+ *
+ */
+package abstractFactoryOrderStyle;
+
+// interface to hold orderStyle behavior
+public interface OrderStyle {
+	public abstract Order createOrder();
+	
+}
